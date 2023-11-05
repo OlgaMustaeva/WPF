da29ecfa107f7ae4c2645d99ec0e70b82e03fc05
OlgaMustaeva <olgaguzina@mail.ru>
2023-11-05 14:25:47 +0500

    Добавьте файлы проекта.

---
+++ Lab9-1.DataBindingDemo/Lab9-1.DataBindingDemo/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab9_1.DataBindingDemo
+{
+
+    public class StudentList : ObservableCollection<Student>
+    { 
+
+    }
+
+
+
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public bool IsEnrolled { get; set; }
+    }
+}
